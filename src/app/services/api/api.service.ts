86ed1248f2be692812204659aed3519ec60de5ed
Alex Sandri <me@alexsandri.com>
2021-03-02 09:54:45 +0100

    Create Api Service

---
+++ src/app/services/api/api.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+
+interface IApiServiceResponse<T>
+{
+  data?: T;
+  errors?: string[];
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private readonly ENDPOINT = "http://localhost:4000";
+
+  constructor() { }
+
+  private async send(method: "DELETE" | "GET" | "PATCH" | "POST", url: string, body?: any): Promise<any>
+  {
+    const response = await fetch(`${this.ENDPOINT}/${url}`, {
+      method,
+      headers: {
+        "Authorization": `Bearer ${localStorage.getItem("user.token")}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+    // No Content
+    if (response.status === 204)
+    {
+      return;
+    }
+
+    const result: IApiServiceResponse<any> = {};
+
+    const json = await response.json();
+
+    if (response.status !== 200)
+    {
+      result.errors = [ json.message ];
+    }
+    else
+    {
+      result.data = json;
+    }
+
+    return result;
+  }
+
+  public async createUser(data: {
+    first_name: string,
+    last_name: string,
+    email: string,
+    password: string,
+  })
+  {
+    return this.send("POST", "users", data);
+  }
+
+  public async createSession(email: string, password: string)
+  {
+    return this.send("POST", "sessions", { email, password });
+  }
+}
