8e574651b6db9d2f8ab02341e8bd96a75678436e
Alex Sandri <me@alexsandri.com>
2021-03-02 14:32:20 +0100

    Create FormGroup for Sign Up

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ApiService } from '../services/api/api.service';
 
 @Component({
@@ -8,6 +9,41 @@ import { ApiService } from '../services/api/api.service';
 })
 export class SignupComponent
 {
+  public readonly form = new FormGroup({
+    first_name: new FormControl(),
+    last_name: new FormControl(),
+    email: new FormControl(),
+    password: new FormControl(),
+  });
+
   constructor(private api: ApiService)
   {}
+
+  public async onSubmit(e: Event)
+  {
+    e.preventDefault();
+
+    const response = await this.api.createUser({
+      first_name: this.form.get("first_name")?.value ?? "",
+      last_name: this.form.get("last_name")?.value ?? "",
+      email: this.form.get("email")?.value ?? "",
+      password: this.form.get("password")?.value ?? "",
+    });
+
+    this.form.get("first_name")?.setErrors({
+      errors: response.errors?.filter(e => e.startsWith(`"first_name"`))
+    });
+
+    this.form.get("last_name")?.setErrors({
+      errors: response.errors?.filter(e => e.startsWith(`"last_name"`))
+    });
+
+    this.form.get("email")?.setErrors({
+      errors: response.errors?.filter(e => e.startsWith(`"email"`))
+    });
+
+    this.form.get("password")?.setErrors({
+      errors: response.errors?.filter(e => e.startsWith(`"password"`))
+    });
+  }
 }
