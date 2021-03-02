dd799844d42dc49053b303b1f2a489200176543e
Alex Sandri <me@alexsandri.com>
2021-03-02 19:26:48 +0100

    Create guards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FeedComponent } from './feed/feed.component';
+import { SignedInGuard } from './guards/signed-in/signed-in.guard';
+import { SignedOutGuard } from './guards/signed-out/signed-out.guard';
 import { HomeComponent } from './home/home.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  { path: "signin", component: SigninComponent },
-  { path: "signup", component: SignupComponent },
-  { path: "feed", component: FeedComponent },
+  { path: "signin", component: SigninComponent, canActivate: [ SignedOutGuard ] },
+  { path: "signup", component: SignupComponent, canActivate: [ SignedOutGuard ] },
+  { path: "feed", component: FeedComponent, canActivate: [ SignedInGuard ] },
   { path: "", component: HomeComponent },
 ];
 
