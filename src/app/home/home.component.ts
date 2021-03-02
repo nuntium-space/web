35bb4f6830612702d67040eaa40ed9e887eb3da9
Alex Sandri <me@alexsandri.com>
2021-03-02 19:40:57 +0100

    Show create an account button only if signed out

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HomeComponent
+{
+  constructor(public auth: AuthService)
+  {}
 }
