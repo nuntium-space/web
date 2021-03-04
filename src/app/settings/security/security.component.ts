145e7a12eb20aae9669e11de83112717f7f2fca3
Alex Sandri <me@alexsandri.com>
2021-03-04 19:35:59 +0100

    Implement change password form

--- src/app/settings/security/security.component.ts
+++ src/app/settings/security/security.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { ApiService } from 'src/app/services/api/api.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
@@ -15,11 +16,34 @@ export class SecurityComponent
     new_password: new FormControl(),
   });
 
-  constructor(private auth: AuthService)
+  constructor(private api: ApiService, private auth: AuthService)
   {}
 
-  public onChangePasswordFormSubmit(e: Event)
+  public async onChangePasswordFormSubmit(e: Event)
   {
     e.preventDefault();
+
+    if (!this.auth.user)
+    {
+      return;
+    }
+
+    const response = await this.api.updateUser(this.auth.user.id, {
+      old_password: this.changePasswordForm.get("old_password")?.value ?? "",
+      new_password: this.changePasswordForm.get("new_password")?.value ?? "",
+    });
+
+    this.changePasswordForm.get("old_password")?.setErrors({
+      errors: response.errors?.filter(e => e.startsWith(`"old_password"`))
+    });
+
+    this.changePasswordForm.get("new_password")?.setErrors({
+      errors: response.errors?.filter(e => e.startsWith(`"new_password"`))
+    });
+
+    if (response.data)
+    {
+      this.changePasswordForm.reset();
+    }
   }
 }
