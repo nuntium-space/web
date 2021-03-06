6c8d6d0bf61d0995cf7ff2708db726251a9dc9b6
Alex Sandri <me@alexsandri.com>
2021-03-06 12:17:45 +0100

    Update onCreateNewPublisherFormSubmit

--- src/app/organization/publishers/create/create.component.ts
+++ src/app/organization/publishers/create/create.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api/api.service';
 
 @Component({
@@ -12,26 +12,41 @@ export class CreatePublisherComponent
 {
   public createNewPublisherForm = new FormGroup({
     name: new FormControl(),
+    url: new FormControl(),
+    image: new FormControl(),
   });
 
-  constructor(private api: ApiService, private router: Router)
+  constructor(private api: ApiService, private router: Router, private route: ActivatedRoute)
   {}
 
   public async onCreateNewPublisherFormSubmit(e: Event)
   {
     e.preventDefault();
 
+    const organization = this.route.snapshot.params.id;
+
     const response = await this.api.createPublisher({
       name: this.createNewPublisherForm.get("name")?.value ?? "",
+      url: this.createNewPublisherForm.get("url")?.value ?? "",
+      image: this.createNewPublisherForm.get("image")?.value ?? "",
+      organization,
     });
 
     this.createNewPublisherForm.get("name")?.setErrors({
       errors: response.errors?.filter(e => e.startsWith(`"name"`))
     });
 
+    this.createNewPublisherForm.get("url")?.setErrors({
+      errors: response.errors?.filter(e => e.startsWith(`"url"`))
+    });
+
+    this.createNewPublisherForm.get("image")?.setErrors({
+      errors: response.errors?.filter(e => e.startsWith(`"image"`))
+    });
+
     if (response.data)
     {
-      this.router.navigateByUrl("/organization/TODO/publishers");
+      this.router.navigateByUrl(`/organization/${organization}/publishers`);
     }
   }
 }
