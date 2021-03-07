d71d0e79d56178302b83abb19dd83f9ba77f6d57
Alex Sandri <me@alexsandri.com>
2021-03-07 09:29:51 +0100

    Implement Organization Details

--- src/app/organization/details/details.component.ts
+++ src/app/organization/details/details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService, IOrganization } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-details',
@@ -7,6 +10,37 @@ import { Component } from '@angular/core';
 })
 export class OrganizationDetailsComponent
 {
-  constructor()
-  {}
+  public organization?: IOrganization;
+
+  public detailsForm = new FormGroup({
+    name: new FormControl(),
+  });
+
+  constructor(private api: ApiService, route: ActivatedRoute)
+  {
+    api.retrieveOrganization(route.snapshot.params.id).then(response =>
+    {
+      this.organization = response.data;
+
+      this.detailsForm.get("name")?.setValue(this.organization?.name);
+    });
+  }
+
+  public async onDetailsFormSubmit(e: Event)
+  {
+    e.preventDefault();
+
+    if (!this.organization)
+    {
+      return;
+    }
+
+    const response = await this.api.updateOrganization(this.organization.id, {
+      name: this.detailsForm.get("name")?.value ?? "",
+    });
+
+    this.detailsForm.get("name")?.setErrors({
+      errors: response.errors?.filter(e => e.startsWith(`"name"`))
+    });
+  }
 }
