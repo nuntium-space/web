3450e14246ee33011cbc82f7b8866dfeae945ab7
Alex Sandri <me@alexsandri.com>
2021-03-07 16:09:41 +0100

    Show publisher name

--- src/app/publisher-public-page/publisher-public-page.component.ts
+++ src/app/publisher-public-page/publisher-public-page.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService, IPublisher } from '../services/api/api.service';
 
 @Component({
   selector: 'app-publisher-public-page',
   templateUrl: './publisher-public-page.component.html',
   styleUrls: ['./publisher-public-page.component.scss']
 })
-export class PublisherPublicPageComponent implements OnInit {
+export class PublisherPublicPageComponent
+{
+  public publisher?: IPublisher;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(api: ApiService, route: ActivatedRoute)
+  {
+    api.retrievePublisher(route.snapshot.url[0].path.replace("~", "")).then(response =>
+    {
+      this.publisher = response.data;
+    });
   }
-
 }
