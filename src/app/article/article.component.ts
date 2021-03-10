97312996afedf4ca073c55a3d35f0dcb9f9b85cc
Alex Sandri <me@alexsandri.com>
2021-03-10 17:33:48 +0100

    Remove unused code and add comment after successful creation

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService, IArticle, IComment } from '../services/api/api.service';
 
@@ -12,13 +11,9 @@ export class ArticleComponent
 {
   public article?: IArticle;
 
-  public comments?: IComment[];
+  public comments: IComment[] = [];
 
-  public addCommentForm = new FormGroup({
-    content: new FormControl(),
-  });
-
-  constructor(private api: ApiService, route: ActivatedRoute)
+  constructor(api: ApiService, route: ActivatedRoute)
   {
     route.params.subscribe({
       next: (params) =>
@@ -30,35 +25,17 @@ export class ArticleComponent
 
         api.listCommentsForArticle(params.id, null).then((response) =>
         {
-          this.comments = response.data;
+          if (response.data)
+          {
+            this.comments = response.data;
+          }
         });
       },
     });
   }
 
-  public async onAddCommentFormSubmit(e: Event)
+  public onCommentCreated(comment: IComment)
   {
-    e.preventDefault();
-
-    if (!this.article)
-    {
-      return;
-    }
-
-    const response = await this.api.createComment(this.article.id, {
-      content: this.addCommentForm.get("content")?.value ?? "",
-      parent: null,
-    });
-
-    this.addCommentForm.get("content")?.setErrors({
-      errors: response.errors?.filter(e => e.startsWith(`"content"`))
-    });
-
-    if (response.data)
-    {
-      this.addCommentForm.reset();
-
-      this.comments?.push(response.data);
-    }
+    this.comments.push(comment);
   }
 }
