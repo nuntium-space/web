51a985ab7887d29fe738711053ce48934fb30eea
Alex Sandri <me@alexsandri.com>
2021-03-15 18:57:40 +0100

    Show articles

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ApiService, IArticle } from '../services/api/api.service';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss']
 })
-export class FeedComponent implements OnInit {
+export class FeedComponent
+{
+  public articles?: IArticle[];
 
-  constructor() { }
+  constructor(api: ApiService, auth: AuthService)
+  {
+    if (!auth.user)
+    {
+      return;
+    }
 
-  ngOnInit(): void {
+    api.retrieveUserFeed(auth.user.id).then(response =>
+    {
+      this.articles = response.data;
+    });
   }
-
 }
