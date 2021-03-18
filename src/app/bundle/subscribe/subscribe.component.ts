24ef60f38a0e19a9513c302029e4cbab8373fa81
Alex Sandri <me@alexsandri.com>
2021-03-18 18:32:36 +0100

    Update subsribe to bundle component

--- src/app/bundle/subscribe/subscribe.component.ts
+++ src/app/bundle/subscribe/subscribe.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService, IBundle } from 'src/app/services/api/api.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-subscribe',
   templateUrl: './subscribe.component.html',
   styleUrls: ['./subscribe.component.scss']
 })
-export class SubscribeComponent implements OnInit {
+export class SubscribeComponent
+{
+  public bundle?: IBundle;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private api: ApiService, private auth: AuthService, route: ActivatedRoute)
+  {
+    route.params.subscribe({
+      next: params =>
+      {
+        api.retrieveBundle(params.id).then(response =>
+        {
+          this.bundle = response.data;
+        });
+      },
+    });
   }
 
+  public async onDetailsFormSubmit(e: Event)
+  {
+    e.preventDefault();
+
+    if (!this.auth.user || !this.bundle)
+    {
+      return;
+    }
+
+    const response = await this.api.subscribeToBundle(this.auth.user.id, this.bundle.id);
+
+    if (response.data)
+    {
+      console.log(response.data);
+    }
+  }
 }
