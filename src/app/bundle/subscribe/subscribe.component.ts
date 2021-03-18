9e1d912ce87b850aa35112367b6546589929fba8
Alex Sandri <me@alexsandri.com>
2021-03-18 18:42:09 +0100

    Init stripe elements card elememt

--- src/app/bundle/subscribe/subscribe.component.ts
+++ src/app/bundle/subscribe/subscribe.component.ts
@@ -1,44 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { loadStripe, Stripe } from '@stripe/stripe-js';
 import { ApiService, IBundle } from 'src/app/services/api/api.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { STRIPE_PUBLISHABLE_KEY } from 'src/config';
 
 @Component({
   selector: 'app-subscribe',
   templateUrl: './subscribe.component.html',
   styleUrls: ['./subscribe.component.scss']
 })
-export class SubscribeComponent
+export class SubscribeComponent implements OnInit
 {
   public bundle?: IBundle;
 
-  constructor(private api: ApiService, private auth: AuthService, route: ActivatedRoute)
+  constructor(private api: ApiService, private auth: AuthService, private route: ActivatedRoute)
+  {}
+
+  public async ngOnInit(): Promise<void>
   {
-    route.params.subscribe({
+    this.route.params.subscribe({
       next: params =>
       {
-        api.retrieveBundle(params.id).then(response =>
+        this.api.retrieveBundle(params.id).then(response =>
         {
           this.bundle = response.data;
         });
       },
     });
+
+    const stripe = await loadStripe(STRIPE_PUBLISHABLE_KEY);
+
+    if (stripe)
+    {
+      const elements = stripe.elements();
+
+      const card = elements.create("card");
+      card.mount("#card-element");
+
+      card.on("change", e =>
+      {
+        console.log(e);
+      });
+    }
   }
 
-  public async onDetailsFormSubmit(e: Event)
+  public async onSubmit(e: Event)
   {
     e.preventDefault();
-
-    if (!this.auth.user || !this.bundle)
-    {
-      return;
-    }
-
-    const response = await this.api.subscribeToBundle(this.auth.user.id, this.bundle.id);
-
-    if (response.data)
-    {
-      console.log(response.data);
-    }
   }
 }
