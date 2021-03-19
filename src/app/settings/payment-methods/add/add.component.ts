5b6d1565660d212e1d2420e3437e5f45a262c76f
Alex Sandri <me@alexsandri.com>
2021-03-19 14:15:55 +0100

    Setup basic functionalitiy of AddPaymentMethodComponent

--- src/app/settings/payment-methods/add/add.component.ts
+++ src/app/settings/payment-methods/add/add.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { loadStripe, Stripe, StripeCardElement } from '@stripe/stripe-js';
+import { ApiService } from 'src/app/services/api/api.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
+import { STRIPE_PUBLISHABLE_KEY } from 'src/config';
 
 @Component({
   selector: 'add-payment-method',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.scss']
 })
-export class AddPaymentMethodComponent
+export class AddPaymentMethodComponent implements OnInit
 {
-  constructor()
+  private stripe: Stripe | null = null;
+
+  private cardElement?: StripeCardElement;
+
+  public cardElementError: string = "";
+
+  constructor(private api: ApiService, private auth: AuthService)
   {}
+
+  public async ngOnInit(): Promise<void>
+  {
+    this.stripe = await loadStripe(STRIPE_PUBLISHABLE_KEY);
+
+    if (this.stripe)
+    {
+      const elements = this.stripe.elements();
+
+      this.cardElement = elements.create("card");
+
+      this.cardElement.mount("#card-element");
+
+      this.cardElement.on("change", e =>
+      {
+        this.cardElementError = e.error?.message ?? "";
+      });
+    }
+  }
+
+  public async onSubmit(e: Event)
+  {
+    e.preventDefault();
+
+    if (!this.stripe || !this.cardElement || !this.auth.user)
+    {
+      return;
+    }
+
+    const result = await this.stripe
+      .createPaymentMethod({
+        type: "card",
+        card: this.cardElement,
+      });
+
+    if (result.error)
+    {
+      console.log(result.error);
+
+      return;
+    }
+
+    // TODO
+  }
 }
