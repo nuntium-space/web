909ab7204ed82b6bfa660c52c94ea03e3e2957ff
Alex Sandri <me@alexsandri.com>
2021-03-19 09:39:34 +0100

    Set payment methods layout

--- src/app/settings/payment-methods/payment-methods.component.ts
+++ src/app/settings/payment-methods/payment-methods.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api/api.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-payment-methods',
   templateUrl: './payment-methods.component.html',
   styleUrls: ['./payment-methods.component.scss']
 })
-export class PaymentMethodsComponent implements OnInit {
+export class PaymentMethodsComponent
+{
+  public paymentMethods?: any[];
 
-  constructor() { }
+  constructor(api: ApiService, auth: AuthService)
+  {
+    if (!auth.user)
+    {
+      return;
+    }
 
-  ngOnInit(): void {
+    /*
+    api.listOrganizationsForUser(auth.user.id).then(response =>
+    {
+      this.organizations = response.data;
+    });
+    */
   }
-
 }
