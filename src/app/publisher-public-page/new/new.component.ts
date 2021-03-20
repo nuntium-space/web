49a627d6ac7e32963554291554d36b773bc61600
Alex Sandri <me@alexsandri.com>
2021-03-20 19:04:39 +0100

    Load all authors for current user to find needed author id (not optimal but for now it's enough)

--- src/app/publisher-public-page/new/new.component.ts
+++ src/app/publisher-public-page/new/new.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ApiService } from 'src/app/services/api/api.service';
+import { ApiService, IAuthor } from 'src/app/services/api/api.service';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'write-new-article',
@@ -10,19 +11,39 @@ import { ApiService } from 'src/app/services/api/api.service';
 })
 export class WriteNewArticleComponent
 {
+  private author?: IAuthor;
+
   public form = new FormGroup({
     title: new FormControl(),
     content: new FormControl(),
   });
 
-  constructor(private api: ApiService, private router: Router, private route: ActivatedRoute)
-  {}
+  constructor(private api: ApiService, private router: Router, private route: ActivatedRoute, auth: AuthService)
+  {
+    if (!auth.user)
+    {
+      return;
+    }
+
+    api.listAuthorsForUser(auth.user.id).then(response =>
+    {
+      if (response.data)
+      {
+        this.author = response.data.find(author => author.publisher.id === route.snapshot.params.id);
+      }
+    });
+  }
 
   public async onSubmit(e: Event)
   {
     e.preventDefault();
 
-    const response = await this.api.createArticle("TODO", {
+    if (!this.author)
+    {
+      return;
+    }
+
+    const response = await this.api.createArticle(this.author.id, {
       title: this.form.get("title")?.value ?? "",
       content: this.form.get("content")?.value ?? "",
     });
