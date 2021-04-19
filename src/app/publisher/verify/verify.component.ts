95060a99f2556efa038f85cc2d0ec2acae0186d2
Alex Sandri <me@alexsandri.com>
2021-04-19 15:03:32 +0200

    Init verify component

--- src/app/publisher/verify/verify.component.ts
+++ src/app/publisher/verify/verify.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService, IPublisher } from 'src/app/services/api/api.service';
 
 @Component({
   selector: 'app-verify-publisher',
   templateUrl: './verify.component.html',
   styleUrls: ['./verify.component.scss']
 })
-export class VerifyPublisherComponent
+export class VerifyPublisherComponent implements OnInit
 {
-  constructor()
+  public publisher?: IPublisher;
+
+  constructor(private api: ApiService, private route: ActivatedRoute)
   {}
+
+  public ngOnInit()
+  {
+    this.route.params.subscribe({
+      next: params =>
+      {
+        this.api.retrievePublisher(params.id).then(response =>
+        {
+          this.publisher = response.data;
+        });
+      },
+    });
+  }
 }
