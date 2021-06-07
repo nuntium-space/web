f11ddb0b2cf44491a871d71c951276b046b68a39
Alex Sandri <me@alexsandri.com>
2021-06-07 09:05:25 +0200

    component is no longer needed

--- src/app/bundle/bundle-routing.module.ts
+++ src/app/bundle/bundle-routing.module.ts
@@ -1,32 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BundleComponent } from './bundle.component';
-import { BundleDetailsComponent } from './details/details.component';
-import { AddPriceComponent } from './prices/add/add.component';
-import { PricesComponent } from './prices/prices.component';
-import { AddPublisherComponent } from './publishers/add/add.component';
-import { BundlePublishersComponent } from './publishers/publishers.component';
-import { SubscribeComponent } from './subscribe/subscribe.component';
 
 const routes: Routes = [
   {
     path: "",
     component: BundleComponent,
     children: [
-      { path: "details", data: { section: "details" }, component: BundleDetailsComponent },
+      { path: "details", data: { section: "details" } },
       {
         path: "prices", data: { section: "prices" },
         children: [
-          { path: "add", component: AddPriceComponent },
-          { path: "", component: PricesComponent },
+          { path: "add" },
+          { path: "" },
         ],
       },
-      { path: "subscribe", data: { section: "subscribe" }, component: SubscribeComponent },
+      { path: "subscribe", data: { section: "subscribe" } },
       {
         path: "publishers", data: { section: "publishers" },
         children: [
-          { path: "add", component: AddPublisherComponent },
-          { path: "", component: BundlePublishersComponent },
+          { path: "add" },
+          { path: "" },
         ],
       },
       { path: "", redirectTo: "details" },
