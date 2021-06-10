26a2d9a5a1dc3d19696b4f92001475ed8d6543b3
Alex Sandri <me@alexsandri.com>
2021-06-10 11:15:56 +0200

    fix

--- src/app/publisher/publisher-routing.module.ts
+++ src/app/publisher/publisher-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignedInGuard } from '../shared/guards/signed-in/signed-in.guard';
+import { PublisherComponent } from './publisher.component';
 
 const routes: Routes = [
   {
     path: "",
+    component: PublisherComponent,
     canActivate: [ SignedInGuard ],
     children: [
       { path: "authors", data: { section: "authors" } },
