adfe780698277aa67e93a1d94a6addef0e81ad52
Alex Sandri <me@alexsandri.com>
2021-06-13 19:41:04 +0200

    Update editor

--- src/app/publisher-public-page/new/new.component.ts
+++ src/app/publisher-public-page/new/new.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Editor } from '@tiptap/core';
+import Underline from '@tiptap/extension-underline';
 import StarterKit from '@tiptap/starter-kit';
 import { ApiService, IAuthor } from 'src/app/services/api/api.service';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
@@ -44,9 +45,32 @@ export class WriteNewArticleComponent implements AfterViewInit
       element: document.querySelector("#editor") ?? undefined,
       extensions: [
         StarterKit,
+        Underline,
       ],
       content: "",
     })
+  }
+
+  public do(action: string, e?: Event)
+  {
+    switch (action)
+    {
+      case "bold": this.editor?.chain().focus().toggleBold().run(); break;
+      case "italic": this.editor?.chain().focus().toggleItalic().run(); break;
+      case "underline": this.editor?.chain().focus().toggleUnderline().run(); break;
+
+      case "style":
+      {
+        const selectElement = e?.target as HTMLSelectElement;
+        const selectedOption = selectElement.selectedOptions[0];
+
+        selectedOption.value === "p"
+          ? this.editor?.chain().focus().setParagraph().run()
+          : this.editor?.chain().focus().toggleHeading({ level: parseInt(selectedOption.value.replace("h", "")) as any }).run();
+
+        break;
+      }
+    }
   }
 
   public async onSubmit(end: () => void)
