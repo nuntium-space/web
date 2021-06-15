fc390a5a5c1647615bd374124f3e1734131755f7
Alex Sandri <me@alexsandri.com>
2021-06-15 13:04:54 +0200

    Use Config.LANGUAGES

--- src/app/settings/preferences/preferences.component.ts
+++ src/app/settings/preferences/preferences.component.ts
@@ -1,33 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
-import { ApiService, ILanguage } from 'src/app/services/api/api.service';
+import { ApiService } from 'src/app/services/api/api.service';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
 import { UserSettingsService } from 'src/app/shared/services/user-settings/user-settings.service';
+import { Config } from 'src/config/Config';
 
 @Component({
   selector: 'settings-preferences',
   templateUrl: './preferences.component.html',
   styleUrls: ['./preferences.component.scss']
 })
-export class PreferencesComponent implements OnInit
+export class PreferencesComponent
 {
   public languageForm = new FormGroup({
     language: new FormControl(this.userSettings.userSettings?.language),
   });
 
-  public languages?: ILanguage[];
+  public languages = Config.LANGUAGES;
 
   constructor(public userSettings: UserSettingsService, private api: ApiService, private auth: AuthService, private translate: TranslateService)
   {}
-
-  ngOnInit(): void
-  {
-    this.api.listSupportedLanguages().then(response =>
-    {
-      this.languages = response.data;
-    });
-  }
 
   public async onLanguageChangeSubmit(end: () => void)
   {
