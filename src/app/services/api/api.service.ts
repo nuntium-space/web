8d4549dbe1da1aaf72442e751c3cd609c1667d04
Alex Sandri <me@alexsandri.com>
2021-06-18 13:24:30 +0200

    Update

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -1,25 +1,6 @@
 import { Injectable } from '@angular/core';
+import { CoreApiService, IApiServiceResponse } from 'src/app/core/services/api/api.service';
 import { Config } from 'src/config/Config';
-import { environment } from 'src/environments/environment';
-
-export interface IApiServiceResponse<T>
-{
-  status: number,
-  success: boolean,
-  /**
-   * Body of the request, if successful
-   */
-  data?: T,
-  errors?: {
-    field: string,
-    error: string,
-    params?: any,
-  }[],
-  /**
-   * Body of the request
-   */
-  raw?: any,
-}
 
 export interface IUser
 {
@@ -140,65 +121,8 @@ export interface IUserSettings
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService
+export class ApiService extends CoreApiService
 {
-  constructor()
-  {}
-
-  private async send(
-    method: "DELETE" | "GET" | "PATCH" | "POST" | "PUT",
-    url: string,
-    body?: any,
-    contentType: "application/json" | "multipart/form-data" = "application/json",
-  ): Promise<IApiServiceResponse<any>>
-  {
-    const headers: HeadersInit = {};
-
-    if (contentType !== "multipart/form-data")
-    {
-      headers["Content-Type"] = contentType;
-    }
-
-    const response = await fetch(`${environment.api.endpoint}/${url}`, {
-      method,
-      headers,
-      body: contentType === "application/json"
-        ? JSON.stringify(body)
-        : body,
-      credentials: "include",
-    });
-
-    const result: IApiServiceResponse<any> = {
-      status: response.status,
-      success: response.status >= 200 && response.status < 300,
-    };
-
-    // No Content
-    if (result.status === 204)
-    {
-      return result;
-    }
-
-    const json = await response.json();
-
-    result.raw = json;
-
-    if (response.status !== 200)
-    {
-      result.errors = json.details;
-    }
-    else
-    {
-      result.data = json;
-    }
-
-    return result;
-  }
-
-  /**
-   * RESOURCES
-   */
-
   public async retrieveTrendingArticles(): Promise<IApiServiceResponse<IArticle[]>>
   {
     return this.send("GET", `articles/trending?expand[]=author&expand[]=author.user&expand[]=author.publisher`);
