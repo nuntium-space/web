cab5a10f4e9235958079acf1fd6e9430fd1ac4ec
Alex Sandri <me@alexsandri.com>
2021-06-20 09:23:44 +0200

    Change language on change event (still not persisted)

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -12,8 +12,17 @@ export class FooterComponent
   public readonly languages = Config.LANGUAGES;
   public currentLanguage;
 
-  constructor(translate: TranslateService)
+  constructor(private translate: TranslateService)
   {
     this.currentLanguage = translate.currentLang;
+  }
+
+  public onLanguageChange(e: Event)
+  {
+    const target = e.target as HTMLSelectElement;
+
+    const lang = target.selectedOptions.item(0)?.value ?? this.currentLanguage;
+
+    this.translate.use(lang);
   }
 }
