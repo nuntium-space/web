e20a292dcfad6fc701482a59ceb6cccf8e30c398
Alex Sandri <me@alexsandri.com>
2021-06-21 20:35:26 +0200

    Add invites page

--- src/app/settings/pages/invites/invites.component.ts
+++ src/app/settings/pages/invites/invites.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IAuthorInvite } from 'src/app/publisher/services/api/api.service';
+import { AuthService } from 'src/app/shared/services/auth/auth.service';
+import { ApiService } from '../../services/api/api.service';
 
 @Component({
   selector: 'settings-invites',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvitesComponent implements OnInit
 {
-  constructor()
+  public invites?: IAuthorInvite[];
+
+  constructor(private api: ApiService, private auth: AuthService)
   {}
 
   public ngOnInit()
-  {}
+  {
+    if (!this.auth.user)
+    {
+      return;
+    }
+
+    this.api
+      .retrieveInvites(this.auth.user.id)
+      .then(response =>
+      {
+        this.invites = response.data;
+      });
+  }
+
+  public async accept(invite: IAuthorInvite)
+  {
+    if (!this.invites)
+    {
+      return;
+    }
+
+    await this.api.acceptInvite(invite.id);
+
+    this.invites = this.invites.filter(_ => _.id !== invite.id);
+  }
 }
