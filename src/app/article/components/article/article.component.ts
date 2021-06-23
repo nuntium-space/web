3a2d6f6015f7b7e6b8610216dd91443977895ca4
Alex Sandri <me@alexsandri.com>
2021-06-23 14:22:43 +0200

    Remove unused code

--- src/app/article/components/article/article.component.ts
+++ src/app/article/components/article/article.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { IArticleDraft } from 'src/app/publisher-public-page/services/api/api.service';
 import { IArticle } from 'src/app/services/api/api.service';
 import { ConfirmEventCallback } from 'src/app/shared/components/form/form.component';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
@@ -22,13 +21,10 @@ export class ArticleComponent implements OnInit
   public isSubscribed = true;
 
   public article?: IArticle;
-  public draft?: IArticleDraft;
 
   public sources?: string[];
 
   public isUpdatingArticle = false;
-
-  public isDraft = false;
 
   constructor(public auth: AuthService, public format: FormatService, public route: ActivatedRoute, private api: ApiService, private router: Router)
   {}
@@ -38,22 +34,6 @@ export class ArticleComponent implements OnInit
     this.route.params.subscribe({
       next: (params) =>
       {
-        this.isDraft = params.id.startsWith("dft_");
-
-        if (this.isDraft)
-        {
-          this.api
-            .retrieveDraft(params.id)
-            .then(response =>
-            {
-              this.draft = response.data;
-
-              this.updateArticleForm.get("title")?.setValue(this.article?.title);
-            });
-
-          return;
-        }
-
         this.api
           .retrieveArticle(params.id)
           .then(response =>
