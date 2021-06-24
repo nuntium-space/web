fcf9d86f341855b9e80d3889e85af7b846cab51b
Alex Sandri <me@alexsandri.com>
2021-06-24 13:06:20 +0200

    Expand drafts

--- src/app/pages/admin/services/api/api.service.ts
+++ src/app/pages/admin/services/api/api.service.ts
@@ -7,6 +7,6 @@ export class ApiService extends CoreApiService
 {
   public async retrieveDraftsSubmittedForVerification(): Promise<IApiServiceResponse<IArticleDraft[]>>
   {
-    return this.send("GET", `__internals/articles/drafts`);
+    return this.send("GET", `__internals/articles/drafts?expand[]=author&expand[]=author.user&expand[]=author.publisher`);
   }
 }
