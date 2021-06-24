f2fb6a50899158e43919310c6a1b597aea171893
Alex Sandri <me@alexsandri.com>
2021-06-24 11:57:41 +0200

    Create admin module

---
+++ src/app/pages/admin/admin-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignedInGuard } from 'src/app/shared/guards/signed-in/signed-in.guard';
+import { AdminComponent } from './admin.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: AdminComponent,
+    canActivate: [ SignedInGuard ],
+  },
+];
+
+@NgModule({
+  imports: [ RouterModule.forChild(routes) ],
+  exports: [ RouterModule ],
+})
+export class AdminRoutingModule
+{}
