2d5a7dc68dae08b9899f72824172492df7b901d4
Alex Sandri <me@alexsandri.com>
2021-07-07 12:15:05 +0200

    Fix: ESLint errors

--- src/app/pages/pages/pages/terms/terms.component.ts
+++ src/app/pages/pages/pages/terms/terms.component.ts
@@ -1,12 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-terms',
+  selector: 'pages-terms',
   templateUrl: './terms.component.html',
   styleUrls: ['./terms.component.scss'],
 })
-export class TermsComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class TermsComponent{
 }
