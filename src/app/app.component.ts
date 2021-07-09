a43cc32ba6611b2c130a8142016be257d734d287
Alex Sandri <me@alexsandri.com>
2021-07-09 13:02:10 +0200

    Add skip to content link for a11y

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, ElementRef } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,9 +10,25 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  public url: string;
+
   private onLangChange?: Subscription;
 
-  constructor(public el: ElementRef, public translate: TranslateService) {}
+  constructor(public el: ElementRef, public translate: TranslateService, private router: Router) {
+    this.url = this.getMainContentUrl();
+
+    router.events
+      .pipe(
+        filter((_) => _ instanceof NavigationEnd),
+      )
+      .subscribe(() => {
+        this.url = this.getMainContentUrl();
+      });
+  }
+
+  private getMainContentUrl(): string {
+    return `${this.router.url.split("#")[0]}#main-content`;
+  }
 
   public ngOnInit(): void {
     this.updateLanguage();
