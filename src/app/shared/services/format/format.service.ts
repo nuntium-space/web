0f04266975f334f782701013c7af2211292921e7
Alex Sandri <me@alexsandri.com>
2021-07-10 19:45:23 +0200

    Use always the currency's symbol

--- src/app/shared/services/format/format.service.ts
+++ src/app/shared/services/format/format.service.ts
@@ -58,6 +58,7 @@ export class FormatService {
     return new Intl.NumberFormat(this.translate.currentLang, {
       style: 'currency',
       currency: currency.toUpperCase(),
+      currencyDisplay: "narrowSymbol",
     }).format(amount);
   }
 }
